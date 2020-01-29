54f62539b987c9f802c7337bf7ce974f5c7ff465
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-29 06:17:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}. "
 end
 
-introduction_with_language('Oliver Queen', 'Python')
+introduction_with_language('Edwin', 'Python')
 
 def introduction_with_language_optional(name, language = 'Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}. "
